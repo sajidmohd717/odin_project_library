51e4a1e554edaec9c36c313a7eb1e6dc45127bbe
Sajid <sajidmohammad460519@gmail.com>
2024-04-30 07:48:08 +0800

    progressing

--- scripit.js
+++ scripit.js
@@ -1,4 +1,15 @@
-const myLibrary = [];
+const myLibrary = [
+  {
+    title : "Tuesdays with Morrie",
+    author: "Mitch Albom",
+    pages: "224"
+  },
+  {
+
+  }
+];
+
+
 
 function Book(title, author, pages) {
   // the constructor...
@@ -12,12 +23,20 @@ function addBookToLibrary() {
   let title = document.querySelector("#title").value;
   let author = document.querySelector("#author").value;
   let pages = document.querySelector("#pages").value;
-  myLibrary.push(new Book(title, author, pages))
-  console.log(myLibrary)
+  myLibrary.push(new Book(title, author, pages));
+  console.log(myLibrary);
 }
 
-let buttonClicked = document.querySelector("#button-lol")
-buttonClicked.addEventListener("click", function() {
-    alert("test")
-    addBookToLibrary()
-})
+let buttonClicked = document.querySelector("#button-lol");
+buttonClicked.addEventListener("click", function () {
+  addBookToLibrary();
+});
+
+myLibrary.forEach((Book) => {
+  const bookDisplayDiv = document.querySelector(".book_display");
+  const bookDiv = document.createElement("div");
+  const titleP = document.createElement("p");
+  titleP.textContent = "Title: " + Book.title;
+  bookDiv.appendChild(titleP);
+  bookDisplayDiv.appendChild(bookDiv);
+});
